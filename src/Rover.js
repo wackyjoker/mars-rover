7ea96d8d39f2bfbc7dee30c78a80b725b0c94f51
wackyjoker <kevin.young8931@gmail.com>
2020-12-24 12:05:23 +0100

    created initial car and mar objects

---
+++ src/Rover.js
@@ -0,0 +1,67 @@
+import { rotationConst } from "./const";
+const rotations = [rotationConst.North, rotationConst.South, rotationConst.East, rotationConst.West];
+const direction = {
+  Left: -1,
+  Right: 1,
+};
+export default class Car {
+  constructor() {}
+
+  init(data) {
+    this.setPosition(data.position);
+    this.setRotation(data.rotation);
+  }
+
+  toDirection(dir) {
+    const currentIndex = this.getCurrentIndexByRotation();
+    let newIndex = (currentIndex + 4 + dir) % rotations.length;
+    let newRotation = rotations[newIndex];
+    this.setRotation(newRotation);
+  }
+
+  toRight() {
+    this.toDirection(direction.Right);
+  }
+
+  toLeft() {
+    this.toDirection(direction.Left);
+  }
+
+  getCurrentIndexByRotation() {
+    return rotations.indexOf(this.rotation);
+  }
+
+  setRotation(rotation) {
+    this.rotation = rotation;
+  }
+
+  getData() {
+    return {
+      position: this.getPosition(),
+      rotation: this.getRotation(),
+    };
+  }
+  getRotation() {
+    return this.rotation;
+  }
+
+  setPosition(position) {
+    this.position = position;
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  setX(val) {
+    this.position.x = val;
+  }
+
+  setY(val) {
+    this.position.y = val;
+  }
+}
+
+export function createCar() {
+  return new Car();
+}
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { createCar } from "./Car";
+import { createMarsMap } from "./MarsMap";
+
+export function exec() {
+  let car = createCar();
+  let map = createMarsMap();
+  execCommand(car, map);
+  return car.getData();
+}
+
+function execCommand(car, map) {
+  for (let index = 0; index < commandList.length; index++) {
+    const command = commandList[index];
+    const result = handleExec(car, map, command);
+    if (result) return;
+  }
+}
+
+function handleExec(car, map, command) {
+  return command.exec(car, map);
+}
+
+let commandList = [];
+export function addCommand(command) {
+  commandList.push(command);
+}
+
+export function size() {
+  return commandList.length;
+}
+
+export function clear() {
+  commandList = [];
+}
